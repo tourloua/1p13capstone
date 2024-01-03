a825cec189c866609714b2ea6d8d14b523047231
tourloua <144152554+tourloua@users.noreply.github.com>
2024-01-03 15:25:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const backButton = document.getElementById('backButton');
 
 if (backButton) {
   backButton.addEventListener('click', function () {
-    window.location.href = 'https://twitter.com/';//change link to project main page
+    window.location.href = 'https://tourloua.github.io/alextourloukis/';
   });
 }
